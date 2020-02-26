2b971e19959f848b36d432aedb5ae98a4d451d48
apple <j7k8l9;0>
2020-02-26 23:25:30 +0800

    problem1,sum of two numbers

---
+++ src/main/java/leetcode/problem1/SolutionOne.java
@@ -0,0 +1,19 @@
+package leetcode.problem1;
+
+/**
+ *
+ * @author dadongge
+ * @date 2020/2/24
+ */
+public class SolutionOne {
+    public int[] twoSum(int[] nums, int target){
+        for( int i = 0; i< nums.length ; i++){
+            for (int j = i+ 1; j < nums.length ; j++){
+                if (nums[j] == target - nums[i]){
+                    return new int[]{i ,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
