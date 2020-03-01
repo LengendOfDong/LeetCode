16df41273723c94ce2c8f4f633e2f36add589328
apple <j7k8l9;0>
2020-03-01 22:17:24 +0800

    add problem3 answer

---
+++ src/main/java/leetcode/problem3/Solution.java
@@ -0,0 +1,53 @@
+package leetcode.problem3;
+
+/**
+ * @author dadongge
+ * @date 2020/2/29
+ */
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        char[] chars = s.toCharArray();
+        String longestStr = "";
+        int max = 0;
+        if(chars.length != 0) {
+            for (char ch : chars) {
+                if (longestStr.indexOf(ch) == -1) {
+                    longestStr = longestStr + ch;
+                } else {
+                    if(max <= longestStr.length()){
+                        max = longestStr.length();
+                    }
+                    if(longestStr.indexOf(ch) != longestStr.length() - 1) {
+                        longestStr = longestStr.substring(longestStr.indexOf(ch) + 1) + ch;
+                    }else {
+                        longestStr = "" + ch;
+                    }
+                }
+            }
+            if(max <= longestStr.length()){
+                max = longestStr.length();
+            }
+            return max;
+        }else{
+            return 0;
+        }
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int a = solution.lengthOfLongestSubstring("abcabcbb");
+        int b = solution.lengthOfLongestSubstring("bbbbb");
+        int c = solution.lengthOfLongestSubstring("pwwkew");
+        int d = solution.lengthOfLongestSubstring("dvdf");
+        int e = solution.lengthOfLongestSubstring(" ");
+        int f = solution.lengthOfLongestSubstring("");
+        int g = solution.lengthOfLongestSubstring("bbtablud");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
---
+++ src/main/java/leetcode/problem3/Test.java
@@ -0,0 +1,60 @@
+package leetcode.problem3;
+
+/**
+ * @author dadongge
+ * @date 2020/2/27
+ */
+public class Test {
+    public int lengthOfLongestSubstring(String s) {
+        Integer maxLength = 0;
+        for(int i = 0; i < s.length() ; i++){
+            int max = getMax(s.substring(i));
+            if(max > maxLength){
+                maxLength = max;
+            }
+        }
+        return maxLength;
+    }
+
+    private int getMax(String s){
+        char[] chars = s.toCharArray();
+        String longestStr = "";
+        int max = 0;
+        if(chars.length != 0){
+            for(char ch : chars){
+                if(longestStr.indexOf(ch) == -1){
+                    longestStr = longestStr + ch;
+                }else{
+                    if(max <= longestStr.length()){
+                        max = longestStr.length();
+                    }
+                    longestStr = "" + ch;
+                }
+            }
+            if(max <= longestStr.length()){
+                max = longestStr.length();
+            }
+        }else{
+            return 0;
+        }
+        return max;
+    }
+
+    public static void main(String[] args){
+        Test solution = new Test();
+        int a = solution.lengthOfLongestSubstring("abcabcbb");
+        int b = solution.lengthOfLongestSubstring("bbbbb");
+        int c = solution.lengthOfLongestSubstring("pwwkew");
+        int d = solution.lengthOfLongestSubstring("dvdf");
+        int e = solution.lengthOfLongestSubstring(" ");
+        int f = solution.lengthOfLongestSubstring("");
+        int g = solution.lengthOfLongestSubstring("bbtablud");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
