66af424dcaf30c3520c72f05589e052107f58287
apple <zheng1dong2@163.com>
2020-03-07 23:52:47 +0800

    add new answer

--- src/main/java/leetcode/最长回文字串_5/Solution.java
+++ src/main/java/leetcode/最长回文字串_5/Solution.java
@@ -1,5 +1,9 @@
 package leetcode.最长回文字串_5;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author dadongge
  * @date 2020/3/3
@@ -21,22 +25,36 @@ public class Solution {
         for(int i= 1;i < s.length()-1 ; i++){
             String s1 = s.substring(0, i);
             String s2 = s.substring(i + 1);
+            String str;
             if(s1.charAt(s1.length() - 1) == s2.charAt(0)){
-                if(s2.charAt(0) == s.charAt(i)){
-                    middle = s1.charAt(s1.length() - 1) + String.valueOf(s.charAt(i)) + s2.charAt(0);
+//                middle = s1.charAt(s1.length() - 1) + String.valueOf(s.charAt(i)) + s2.charAt(0);
+                if(s1.length() >= 1 && s2.length() >= 1){
+                    str = getStr(String.valueOf(s.charAt(i)), s1, s2);
                 }else{
-                    middle = String.valueOf(s.charAt(i));
+                    middle = s1.charAt(s1.length() - 1) + String.valueOf(s.charAt(i)) + s2.charAt(0);
+
                 }
             }else {
                 if(s2.charAt(0) == s.charAt(i)){
                     middle = String.valueOf(s.charAt(i)) + s2.charAt(0);
+                    if(s2.length() > 1){
+                        s2 = s2.substring(1);
+                        str = getStr(middle, s1, s2);
+                    }else{
+                        str = middle;
+                    }
                 }else if(s1.charAt(s1.length() - 1) == s.charAt(i)){
                     middle = s1.charAt(s1.length() - 1) + String.valueOf(s.charAt(i));
+                    if(s1.length() > 1){
+                        s1 = s1.substring(0,s1.length() - 1);
+                        str = getStr(middle, s1, s2);
+                    }else{
+                        str = middle;
+                    }
                 }else {
-                    continue;
+                    str = String.valueOf(s.charAt(i));
                 }
             }
-            String str = getStr(middle, s1, s2);
             if(longestReverseStr.length() <= str.length()){
                 longestReverseStr = str;
             }
@@ -46,16 +64,17 @@ public class Solution {
 
     public String getStr(String s,String s1,String s2){
         if(s1.length() >1 && s2.length() > 1) {
-            s = s1.substring(0, s1.length() - 1) + s + s2.substring(1);
-            s1 = s1.substring(0, s1.length() - 1);
-            s2 = s2.substring(1);
-        }else if(s1.length() == 1 && s2.length() == 1) {
-            return s1 + s + s2;
-        }else if(s2.length()  == 1) {
-            return s + s2;
-        }else if(s1.length() == 1){
-            return s1 + s;
+            if(s1.charAt(s1.length() - 1) == s2.charAt(0)) {
+                s = s1.charAt(s1.length() - 1) + s + s2.charAt(0);
+                s1 = s1.substring(0, s1.length() - 1);
+                s2 = s2.substring(1);
+            }else {
+                return s;
+            }
         }else{
+            if(s1.charAt(s1.length() - 1) == s2.charAt(0)){
+                s = s1.charAt(s1.length() - 1) + s + s2.charAt(0);
+            }
             return s;
         }
         return getStr(s,s1,s2);
@@ -63,7 +82,12 @@ public class Solution {
 
     public static void main(String[] args){
         Solution solution = new Solution();
-        String s = solution.longestPalindrome("aaa");
+//        List<String> array = Arrays.asList("a","ab","aa","aba","aaa","aab","baa","abc","abcd","abba","abbc","aabc","bcaa","bacab","bacad","aaaa");
+//        for(String str:array){
+//            String s = solution.longestPalindrome(str);
+//            System.out.println(s);
+//        }
+        String s = solution.longestPalindrome("aaaa");
         System.out.println(s);
     }
 }
