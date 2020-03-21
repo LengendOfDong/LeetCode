6e5d589d459d820c611e5a2fc448989d6ede5207
apple <zheng1dong2@163.com>
2020-03-21 23:12:29 +0800

    add new Answers

---
+++ src/main/java/leetcode/Z字型变换/Solution.java
@@ -0,0 +1,52 @@
+package leetcode.Z字型变换;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author dadongge
+ * @date 2020/3/21
+ */
+public class Solution {
+    public String convert(String s,int numRows){
+        Map<Integer,String> map = new LinkedHashMap<>();
+        int k = 1;
+        boolean flag = true;
+        for(char c: s.toCharArray()){
+            if(map.containsKey(k)) {
+                map.put(k,map.get(k) + c);
+            }else{
+                map.put(k,c+"");
+            }
+            if(!flag){
+                if(k == 1){
+                    k++;
+                    flag=true;
+                }else{
+                    k--;
+                }
+            }else{
+                if(k == numRows){
+                    k--;
+                    flag=false;
+                }else{
+                    k++;
+                }
+            }
+        }
+
+        String convertString = "";
+        for(Map.Entry<Integer,String> entry:map.entrySet()){
+            convertString = convertString + entry.getValue();
+        }
+        return convertString;
+
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        String leetcodeishiring = solution.convert("LEETCODEISHIRING", 4);
+        System.out.println(leetcodeishiring);
+    }
+}
--- src/main/java/leetcode/最小的K个数/Solution.java
+++ src/main/java/leetcode/最小的K个数/Solution.java
@@ -1,11 +1,34 @@
 package leetcode.最小的K个数;
 
+import java.util.Arrays;
+
 /**
  * @author dadongge
  * @date 2020/3/20
  */
 public class Solution {
-    public int[] getLeastNumbers(int[] arr, int k){
-        
+    public int[] getLeastNumbers(int[] arr, int k) {
+        for (int i = 0; i < k; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        int[] array = new int[k];
+        System.arraycopy(arr, 0, array, 0, k);
+        return array;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr1 = new int[]{3, 2, 1};
+        int[] leastNumbers = solution.getLeastNumbers(arr1, 2);
+        System.out.println(Arrays.toString(leastNumbers));
+        int[] arr2 = new int[]{0, 1, 2, 1};
+        int[] leastNumbers1 = solution.getLeastNumbers(arr2, 1);
+        System.out.println(Arrays.toString(leastNumbers1));
     }
 }
